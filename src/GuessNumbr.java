5f9a97e0807117a3a720400eec94056dfc74a32a
hongyxf <107297286+hongyxf@users.noreply.github.com>
2022-06-15 11:09:45 +0800

    这是GuessNumber.java文件

---
+++ src/GuessNumbr.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumbr {
+
+
+
+
+
+
+        public static void main(String[] args) {
+
+            /*Random r = new Random();
+            int num = r.nextInt(100) + 1;
+            Scanner sc = new Scanner(System.in);
+            for (int i = 0; i < 7; i++) {
+                System.out.println("请输入您猜的数字：");
+                int guess = sc.nextInt();
+
+                if (i == 7) {
+                    if (guess == num) {
+                        System.out.println("恭喜你，第7次终于猜对啦！");
+                    } else {
+                        System.out.println("很遗憾！次数用完");
+                        System.out.println("正确答案是：" + num);
+                        break;
+                    }
+                }
+                if (guess > num) {
+                    System.out.println("你猜得太大了！请重试");
+                } else if (guess < num) {
+                    System.out.println("你猜得太小了！请重试");
+                } else {
+                    System.out.println("恭喜你，你猜对啦！");
+                    System.out.println("你一共猜了" + i + "次，继续加油！");
+                    break;
+
+
+                }
+
+            }
+            System.out.println("游戏结束");
+        }*/
+        }
+    }
+
+
+
+
+
